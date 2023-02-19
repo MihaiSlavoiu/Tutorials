41f3f1105cdc9efef80c9b2734e56e7e57577346
Mihai Slavoiu <mihai@qoffice.ro>
2023-02-19 19:18:58 +0200

    Added Selenium Project

---
+++ Selenium/src/main/java/selenium/UdemyAlerts.java
@@ -0,0 +1,27 @@
+package selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UdemyAlerts {
+    public static void main(String[] args) throws InterruptedException {
+        String text = "Mihai";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        //Thread.sleep(2000);
+        driver.findElement(By.id("name")).sendKeys(text);
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+
+
+
+    }
+}
