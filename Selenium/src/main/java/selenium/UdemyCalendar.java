35323a34f39aa678a187b169e39b45619733edaf
Mihai Slavoiu <mihai@qoffice.ro>
2023-02-19 16:03:36 +0200

    Added Selenium Project

--- Selenium/src/main/java/selenium/UdemyCalendar.java
+++ Selenium/src/main/java/selenium/UdemyCalendar.java
@@ -3,6 +3,7 @@ package selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UdemyCalendar {
     public static void main(String[] args) throws InterruptedException {
@@ -17,5 +18,13 @@ public class UdemyCalendar {
         System.out.println(driver.findElement(By.id("ctl00_mainContent_view_date2")).isEnabled());
         driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
         System.out.println(driver.findElement(By.id("ctl00_mainContent_view_date2")).isEnabled());
+
+        System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+        if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1")){
+            System.out.println("its true");
+            Assert.assertTrue(true);
+        }else{
+            Assert.assertTrue(false);
+        }
     }
 }
