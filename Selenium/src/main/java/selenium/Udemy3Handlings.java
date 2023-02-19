a4c08e7e439e812d228ea30ed50f69eb3f246736
Mihai Slavoiu <mihai@qoffice.ro>
2023-02-19 15:30:50 +0200

    Added Selenium Project

---
+++ Selenium/src/main/java/selenium/Udemy3Handlings.java
@@ -0,0 +1,74 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class Udemy3Handlings {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        //HANDLING STATIC DROPDOWNS - with select tab
+//        WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+//        Select dropdown = new Select(staticDropdown);
+//        dropdown.selectByIndex(3);
+//        System.out.println(dropdown.getFirstSelectedOption().getText());
+//        dropdown.selectByVisibleText("AED");
+//        System.out.println(dropdown.getFirstSelectedOption().getText());
+//        dropdown.selectByValue("INR");
+//        System.out.println(dropdown.getFirstSelectedOption().getText());
+
+        //HANDLING STATIC DROPDOWNS 2
+//        driver.findElement(By.id("divpaxinfo")).click();
+//        Thread.sleep(2000);
+//        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+//        /*int i=1;
+//        while(i<5){
+//        driver.findElement(By.id("hrefIncAdt")).click();//4 times
+//        i++;
+//        }*/
+//        for (int i = 0; i <= 3; i++) {
+//            driver.findElement(By.id("hrefIncAdt")).click();
+//        }
+//        //driver.findElement(By.id("hrefIncAdt")).click(); // aici baga doar 1 adult in plus
+//        driver.findElement(By.id("btnclosepaxoption")).click();
+//        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+        //HANDLING DYNAMIC DROPDOWNS
+//        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+//        Thread.sleep(2000);
+//        driver.findElement(By.cssSelector("a[value='AIP']")).click();
+//        Thread.sleep(2000);
+//        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); // sau //div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA'] - parent relationship xpath, pui spatiu intre xpath uri
+
+        //HANDLING AUTOSUGGESTIVE DROPDOWNS
+//        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+//        Thread.sleep(2000);
+//        List<WebElement> dropdownOptions = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+//        for (WebElement option : dropdownOptions) {
+//            if (option.getText().equals("India")) {
+//                option.click();
+//                break;
+//            }
+//        }
+
+        //HANDLING CHECKBOX AND SIZE OF THEM
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+
+        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+        driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+
+        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+        //count the number of checkboxes
+        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
+    }
+}
--- Selenium/src/main/java/selenium/UdemyWindowsActivities.java
+++ Selenium/src/main/java/selenium/UdemyWindowsActivities.java
@@ -6,8 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class UdemyWindowsActivities {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.manage().window().maximize(); //maximizeaza pagina
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");//aici asteapta pana pagina e incarcata 100%
+        driver.navigate().to("https://google.com/");//aici asteapta pana se incarca primul element
         driver.navigate().back();
         driver.navigate().forward();
     }
