944e6418c09cbb25e338b33e4749e48e68933bd7
Mihai Slavoiu <mihai@qoffice.ro>
2023-02-22 22:39:33 +0200

    Added Selenium Project

--- Selenium/src/main/java/selenium/Udemy14TableSortingJavaStream.java
+++ Selenium/src/main/java/selenium/Udemy14TableSortingJavaStream.java
@@ -1,24 +1,60 @@
+//Build Custom Selenium methods using Streams Mapper
 package selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
 import java.util.List;
+
 import java.util.stream.Collectors;
 
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+
+
 public class Udemy14TableSortingJavaStream {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.get("https://rahulshettyacademy.com/greenkart/#/offers");
+// click on column
         driver.findElement(By.xpath("//tr/th[1]")).click();
-        List<WebElement> listaLegume = driver.findElements(By.xpath("//td[1]"));
-        List<String> listaOriginala = listaLegume.stream().map(s->s.getText()).collect(Collectors.toList());
-        List<String> listaSortata = listaOriginala.stream().sorted().collect(Collectors.toList());
-        Assert.assertTrue(listaOriginala.equals(listaSortata));
-        listaLegume.stream().map(s->getPriceOfVeggie())
+
+// capture all webelements into list
+        List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
+
+// capture text of all webelements into new(original) list
+        List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
+
+// sort on the original list of step 3 -> sorted list
+        List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
+
+// compare original list vs sorted list
+        Assert.assertTrue(originalList.equals(sortedList));
+        List<String> price;
+
+// scan the name column with getText ->Beans->print the price of the Rice
+//Automating Pagination Scenarios to search the data using do while loop
+        do {
+            List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
+            price = rows.stream().filter(s -> s.getText().contains("Rice"))
+                    .map(s -> getPriceVeggie(s)).collect(Collectors.toList());
+            price.forEach(a -> System.out.println(a));
+            if (price.size() < 1) {
+                driver.findElement(By.cssSelector("[aria-label='Next']")).click();
+            }
+        } while (price.size() < 1);
+    }
+    private static String getPriceVeggie(WebElement s) {
+        String pricevalue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+        return pricevalue;
     }
 }
