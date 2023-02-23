528f8ed97276d3cacd577c3fbe0d6929afc2c7a0
Mihai Slavoiu <mihai@qoffice.ro>
2023-02-24 00:01:21 +0200

    Add

---
+++ TestNgTut/src/test/java/org/example/test/day1.java
@@ -0,0 +1,39 @@
+package org.example.test;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import static org.testng.TestNGAntTask.Mode.junit;
+
+public class day1 {
+
+
+    // TODO Auto-generated method stub
+    @AfterTest
+    public void lastexecution()
+    {
+        System.out.println("I will execute last");
+
+    }
+    @Test
+    public void Demo()
+    {
+        System.out.println("hello");//automation
+        Assert.assertTrue(true);
+    }
+    @AfterSuite
+
+    public void afSyite()
+    {
+        System.out.println("I am the no 1 from last ");
+    }
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("bye");
+    }
+
+
+
+}
